794404a6a469afbbfc6ff90480063c93c81a7c24
吴海龙 <1057286357@qq.com>
2021-07-20 15:05:41 +0800

    qidongye

--- app/src/main/java/com/example/spg/MainActivity.java
+++ app/src/main/java/com/example/spg/MainActivity.java
@@ -3,7 +3,11 @@ package com.example.spg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +15,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LinearLayout linearLayout =(LinearLayout)findViewById(R.id.img_lay);
+        linearLayout.setSystemUiVisibility(View.INVISIBLE);  //设置不可见
+        //延迟加载主页面
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, Sy.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+
+            }
+        }, 2000);
     }
 }
