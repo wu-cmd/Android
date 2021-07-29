5a38a4332a9a20c11741517cfb50d21f7d1c10ed
吴海龙 <1057286357@qq.com>
2021-07-29 18:02:03 +0800

    bugbao

--- app/src/main/java/module/Sy/Sy.java
+++ app/src/main/java/module/Sy/Sy.java
@@ -5,19 +5,48 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.spg.R;
+import com.zhouwei.mzbanner.MZBannerView;
+import com.zhouwei.mzbanner.holder.MZViewHolder;
 
 public class Sy extends Fragment {
-
+protected MZBannerView mMZBanner;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate (R.layout.activity_sy2,container,false);
+        mMZBanner = (MZBannerView) view.findViewById(R.id.banner);
 
+        // 设置数据
+        mMZBanner.setPages(list, new MZHolderCreator<BannerViewHolder>() {
+            @Override
+            public BannerViewHolder createViewHolder() {
+                return new BannerViewHolder();
+            }
+        });
+
+        public class BannerViewHolder implements MZViewHolder<Integer> {
+            private ImageView mImageView;
+            @Override
+            public View createView(Context context) {
+                // 返回页面布局
+                View view = LayoutInflater.from(context).inflate(R.layout.banner_item,null);
+                mImageView = (ImageView) view.findViewById(R.id.banner_image);
+                return view;
+            }
+
+            @Override
+            public void onBind(Context context, int position, Integer data) {
+                // 数据绑定
+                mImageView.setImageResource(data);
+            }
+        }
     }
 }
--- app/src/main/java/module/login/register.java
+++ app/src/main/java/module/login/register.java
@@ -3,15 +3,31 @@ package module.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.spg.MainActivity;
 import com.example.spg.R;
+import com.example.spg.Tabbarmian;
 
-public class register extends Activity {
+import module.Sy.Sy;
+
+public class register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Button login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(register.this, Tabbarmian.class);
+                startActivity(intent);
+            }
+        });
     }
 }
