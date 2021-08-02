2375f32c78806d0b34c7e20f8ee4fb36f79cdc3c
吴海龙 <1057286357@qq.com>
2021-08-02 09:51:31 +0800

    logind

--- app/src/main/java/module/Sy/Sy.java
+++ app/src/main/java/module/Sy/Sy.java
@@ -4,14 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
 import com.example.spg.R;
-
 
 public class Sy extends Fragment {
 
@@ -19,7 +18,6 @@ public class Sy extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate (R.layout.activity_sy2,container,false);
-
 
     }
 }
